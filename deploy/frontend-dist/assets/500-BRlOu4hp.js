b8d844e1d2f490ad5f0ec175a157d6f007b5d77b
X-Panel Central <dev@xpanel.local>
2026-09-15 17:55:02 +0800

    chore: publish balanced dashboard cards

--- deploy/frontend-dist/assets/500-BRlOu4hp.js
+++
@@ -1,1 +0,0 @@
-import{aa as e,ea as t,ma as n}from"./router-KynOkYry.js";import{t as r}from"./exception-base-DwaOSeYe.js";var i=e({name:`500`,__name:`index`,setup(e){return(e,i)=>{let a=r;return n(),t(a,{type:`500`})}}});export{i as default};
